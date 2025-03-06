f325e492958c54c951757d4950d96ad7c3cb9c40
Oguzhan Güner <s158897rstudent.ap.be>
2025-03-06 00:01:17 +0000

    Add Level page styling and scripting, creating new page Projects and improving it

---
+++ RingBearers/projects.js
@@ -0,0 +1,33 @@
+let photos = Array.from(document.getElementsByClassName("project-photos"));
+
+function setBackground(imageUrl) {
+    document.body.style.background = `url('${imageUrl}') no-repeat`;
+    document.body.style.backgroundSize = "cover";
+}
+
+function resetBackground() {
+    document.body.style.background = "";
+}
+
+let backgrounds = [
+    './images/projects/fifa-background.jpg',
+    './images/projects/fornite-background.jpg',
+    './images/projects/legomaster-background.webp',
+    './images/projects/lotr-background.jpg',
+    './images/projects/mtg-background.jpg',
+    './images/projects/pokemon-background.jpg'
+];
+
+photos.forEach((photo, i) => {
+    photo.addEventListener("mouseenter", function() {
+        setBackground(backgrounds[i]);
+    });
+
+    photo.addEventListener("mouseleave", resetBackground);
+
+    photo.addEventListener("click", function() {
+        if (photo !== photos[3]) {
+            alert("This option is not available");
+        }
+    });
+});
